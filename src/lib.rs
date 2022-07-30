23edf27dcdaa875ab2cb219508aebdcc90e97696
Ego_Renegade <eziorevoltez@gmail.com>
2022-07-30 09:43:37 +0100

    refactored to rust naming conventions

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 //! A Fast and Reactive Distributed Systems ToolBox
 //!
 //! RustNet is a set of tools to make building distributed systems easier
-mod Delivery;
+mod delivery;
 mod failure_detectors;
 mod node;
+pub use delivery::{BestEffortDelivery, ReliableDelivery, UniformReliableDelivery};
 pub use failure_detectors::failure_detector;
 pub use node::Node;
 pub use types::{ComponentChannels, Message, MessageType, NetComponent, NetComponents};
-pub use Delivery::{BestEffortDelivery, ReliableDelivery, UniformReliableDelivery};
 mod types;
 #[cfg(test)]
 mod tests {
